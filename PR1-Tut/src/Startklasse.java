64923cc3cf66ac7d6c76d656306b1db2c348dc5c
Selim Karabulut <selim.karabulut@hotmail.de>
2018-10-15 15:13:48 +0200

    Third Commit!

--- PR1-Tut/src/Startklasse.java
+++ PR1-Tut/src/Startklasse.java
@@ -5,6 +5,7 @@ public class Startklasse {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello Germany");
+		System.out.println("Hello France");
 	}
 
 }
